8a9bc9ef4c43ac5d31ad703a2f65d827fd653a2d
ojoven <mikeltorresmail@gmail.com>
2014-12-20 12:26:47 +0100

    Vendor added

--- config/settings.php
+++ config/settings.php
@@ -6,5 +6,9 @@ define('DB_USERNAME','xxxx');
 define('DB_PASSWORD','xxxx');
 define('DB_NAME','xxxx');
 
+// Path Settings
+define('INDEX_PATH', str_replace('\\', '/', dirname(__FILE__)));
+define('APPLICATION_PATH', str_replace('\\', '/', realpath(INDEX_PATH . '/../')));
+
 // Site Settings
 define('MAX_NUM_EPISODES', 10);
--- index.php
+++ index.php
@@ -1,34 +1,57 @@
 <?php
+// Settings
 require_once 'config/settings.php';
-require_once 'lib/functions.php';
-require_once 'lib/vendor/MysqliDb/MysqliDb.php';
 
+// Lib
+require_once 'lib/functions.php';
+require_once 'lib/vendor/SimpleImage.php';
+require_once 'lib/vendor/amazon-s3-php-class/S3.php';
+
+echo APPLICATION_PATH;
+
+return;
+
+// Connect to DB
 $db = new MysqliDb (DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Get All Podcasts
 $podcasts = $db->get('podcasts');
 
-$podcastRssUrl = "http://radiofrance-podcast.net/podcast09/rss_10078.xml";
-//$xmlPodcast = file_get_contents($podcastRssUrl);
-//$xml = new SimpleXMLElement($xmlPodcast);
-
-// Retrieve episodes' mp3s from the XML
-$podcast = json_decode(file_get_contents('json'),true);
-
-// Retrieve podcast and episodes info
-$title = $podcast['channel']['title'];
-$image = $podcast['channel']['image']['url'];
-$episodes = $podcast['channel']['item'];
-
-$episodes = array_slice($episodes,0,MAX_NUM_EPISODES);
-
-foreach ($episodes as $episode) {
-
-	$mp3 = $episode['enclosure']['@attributes'];
-	$mp3['title'] = $episode['title'];
-	$mp3['slug'] = url_slug($episode['title']);
-	$mp3['date'] = $episode['pubDate'];
+// Retrieve the episodes for the podcasts
+foreach ($podcasts as $podcast) {
 	
-	print_r($mp3);
+	$podcastRssUrl = $podcast['url'];
+	$xmlPodcast = file_get_contents($podcastRssUrl);
+	$podcast = new SimpleXMLElement($xmlPodcast);
+	
+	// Retrieve podcast and episodes info
+	$title = $podcast['channel']['title'];
+	$image = $podcast['channel']['image']['url'];
+	$episodes = $podcast['channel']['item'];
+	
+	if (!empty($episodes)) {
 
+		// If the podcast is not parsed yet, we'll save its image
+		
+		
+		// Max num of episodes
+		$episodes = array_slice($episodes,0,MAX_NUM_EPISODES);
+		
+		foreach ($episodes as $episode) {
+		
+			$mp3 = $episode['enclosure']['@attributes'];
+			$mp3['title'] = $episode['title'];
+			$mp3['slug'] = url_slug($episode['title']);
+			$mp3['date'] = $episode['pubDate'];
+		
+		}
+		
+	}
+	
+	
 }
+
+
+
 
 // Foreach episode run sox
